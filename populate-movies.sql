e913653b0087427a60f198cab5bfbed7caf01812
Kumara Swamy Barapati <kbarapati00@mylangara.ca>
2025-10-03 02:42:57 +0000

    second commit

---
+++ populate-movies.sql
@@ -0,0 +1,4 @@
+INSERT INTO movies(name, director, year, genre) VALUES 
+('Cherasaala', 'Ram Prakash Gunnam', 2025, 'Drama'), 
+('Bhairavam', 'Vijay Kanakamedala', 2025, 'Drama'),
+('Ghatikachalam', 'Amar Kamepalli', 2025, 'Thriller');
