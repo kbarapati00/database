4563d8ed434e50f0fae270b76ddc9fcec71c9d98
Kumara Swamy Barapati <kbarapati00@mylangara.ca>
2025-10-03 02:53:29 +0000

    third commit

--- populate-movies.sql
+++ populate-movies.sql
@@ -1,3 +1,7 @@
+-- Create a new file called populate-movies.sql that adds three movies to the above
+-- table, with data for each column. (You can choose your favorite movies!)
+
+
 INSERT INTO movies(name, director, year, genre) VALUES 
 ('Cherasaala', 'Ram Prakash Gunnam', 2025, 'Drama'), 
 ('Bhairavam', 'Vijay Kanakamedala', 2025, 'Drama'),
