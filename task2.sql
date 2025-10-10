e5c559e5b71aa28d4aa21f2b0b1fd0bf92b790b6
Kumara Swamy Barapati <kbarapati00@mylangara.ca>
2025-10-10 03:39:53 +0000

    second task done

--- task2.sql
+++ task2.sql
@@ -21,5 +21,29 @@ author_id IN (SELECT id FROM authors WHERE name='Jon Fosse'));
 -- names of the columns.
 
 -- 1 c
+SELECT MAX(books_count) AS "Most Books", author_id FROM (SELECT COUNT(author_id) AS books_count, author_id FROM authored GROUP BY author_id);
 
-SELECT id FROM authors;
+
+
+-- d) Your next task is to write a series of queries to get the name of the author with the
+-- most books. You've already done some of this work in the previous task; now you
+-- just have to look up the ID in the authors table! An easy way to do this is to create a
+-- temporary table to store the ID of the author, and then use that temporary table in
+-- a separate query to get the name.
+-- • Start by creating a temporary table like this:
+-- CREATE TEMP TABLE IF NOT EXISTS "MostBooks" AS
+-- <query to get the ID of the author with the most books>
+-- The "IF NOT EXISTS" part prevents query from being run if the table name
+-- already exists.
+-- • Congrats! You now have a temporary table called MostBooks, from which
+-- you can query the ID of the author with the most books. Now figure out how
+-- to use this temp table to query the name of the author with the most books.
+
+-- 2 -d
+
+
+CREATE TEMP TABLE IF NOT EXISTS "MostBooks" AS 
+SELECT author_id FROM (SELECT MAX(books_count) AS "Most Books", author_id FROM (SELECT COUNT(author_id) AS books_count, author_id FROM authored GROUP BY author_id));
+
+
+SELECT name AS "Author with Most Books" FROM authors WHERE id IN ( SELECT author_id FROM MostBooks );
